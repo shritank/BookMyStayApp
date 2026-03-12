e43f02212f5a70060fece15dc7efba20161a2d5b
Falcon <shritank@gmail.com>
2026-03-12 15:08:40 +0530

    UC3: centralized room inventory management system using hash map

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-public class Main {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Main
+{
 
     public static void main(String[] args) {
 
@@ -6,18 +10,46 @@ public class Main {
         System.out.println("Welcome to BookMyStayApp!");
         System.out.println("Your simple hotel booking system\n");
 
-        // UC2 - Display Available Rooms
-        System.out.println("Available Rooms:");
+        // UC3 - Centralized Room Inventory
+        RoomInventory inventory = new RoomInventory();
 
-        String[] rooms = {
-                "Room 101 - Single Room",
-                "Room 102 - Double Room",
-                "Room 201 - Deluxe Room",
-                "Room 202 - Suite Room"
-        };
+        System.out.println("Current Room Availability:");
+        inventory.displayInventory();
+    }
+}
 
-        for (String room : rooms) {
-            System.out.println(room);
+/*
+ UC3 - RoomInventory Actor
+ Responsible for centralized management of room availability
+*/
+class RoomInventory {
+
+    private HashMap<String, Integer> roomAvailability;
+
+    // Constructor initializes room inventory
+    public RoomInventory() {
+        roomAvailability = new HashMap<>();
+
+        roomAvailability.put("Single Room", 5);
+        roomAvailability.put("Double Room", 4);
+        roomAvailability.put("Deluxe Room", 3);
+        roomAvailability.put("Suite Room", 2);
+    }
+
+    // Retrieve availability
+    public int getAvailability(String roomType) {
+        return roomAvailability.getOrDefault(roomType, 0);
+    }
+
+    // Update availability
+    public void updateAvailability(String roomType, int newCount) {
+        roomAvailability.put(roomType, newCount);
+    }
+
+    // Display inventory
+    public void displayInventory() {
+        for (Map.Entry<String, Integer> entry : roomAvailability.entrySet()) {
+            System.out.println(entry.getKey() + " : " + entry.getValue() + " rooms available");
         }
     }
 }
