1965a85adf9937b37df1e65c14959e3ee00b49d9
Falcon <shritank@gmail.com>
2026-03-12 14:34:59 +0530

    UC1: Welcome message for BookMyStayApp

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+/**
+ * BookMyStayApp
+ *
+ * Entry point for the Hotel Booking application.
+ * Demonstrates how a Java program starts and prints a welcome message.
+ *
+ * @author Falcon
+ * @version 1.0
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("==================================");
+        System.out.println("        Book My Stay App          ");
+        System.out.println("      Hotel Booking System        ");
+        System.out.println("           Version 1.0            ");
+        System.out.println("==================================");
+
+        System.out.println("Welcome to the Hotel Booking System!");
+    }
+}
